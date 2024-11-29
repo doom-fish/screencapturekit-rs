00a0b6fc8ca29fcef7980c5971420430e82f0bac
Per Johansson <per@doom.fish>
2024-11-29 14:58:28 +0100

    feat!: 3.0 is here with video capture support

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -1,1 +1,4 @@
 pub mod sc_stream_frame_info;
+
+pub use core_media_rs::cm_sample_buffer::{CMSampleBuffer, CMSampleBufferRef};
+pub use core_video_rs::cv_pixel_buffer::{lock::*, CVPixelBuffer, CVPixelBufferRef};
